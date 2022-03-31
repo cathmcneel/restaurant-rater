702c7832d5dbd3cc59c0b3c7bba49ab4a23cd66e
Catherine McNeel <catherinemcneel@icloud.com>
2022-03-30 23:00:31 -0400

    file issue

--- utils/helpers 2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    }
-  };
-  
